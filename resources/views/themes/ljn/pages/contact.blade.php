6b1e634059d2746abc4cd440057c6663a3269a48
Agung <muhammadagunghartono@gmail.com>
2022-05-25 16:01:51 +0700

    add map ljn

--- resources/views/themes/ljn/pages/contact.blade.php
+++ resources/views/themes/ljn/pages/contact.blade.php
@@ -9,6 +9,11 @@
                     <p>Jl. Bahagia No.40, Panjunan, Kec. Lemahwungkuk, Kota Cirebon, Jawa Barat 45112</p>
                 </div>
             </div>
+            <div class="row">
+                <div class="col mb-3">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3962.4422677152743!2d108.5646501147717!3d-6.715759295144873!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6ee3982058fb49%3A0xc52ac0e239efaa99!2sPT.%20Lintas%20Jaringan%20Nusantara%20(LJN%20Cirebon)!5e0!3m2!1sid!2sid!4v1653468883643!5m2!1sid!2sid" width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
             <div class="row mb-3">
                 <div class="col-md-3">
                     <div class="contact_info">
